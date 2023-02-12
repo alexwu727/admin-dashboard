0fab261e120b7982f90917634e3d4b311abecb5b
Alex Wu <alexwu727@gmail.com>
2023-02-12 15:07:04 -0800

    build navbar with MUI

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout"
 import Dashboard from "scenes/dashboard";
+
+
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
