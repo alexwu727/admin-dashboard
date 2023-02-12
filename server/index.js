531ab94d9dc7c67aa12e018aa22e252d874c44a7
Alex Wu <alexwu727@gmail.com>
2023-02-12 10:27:51 -0800

    mongoose setup

--- server/index.js
+++ server/index.js
@@ -27,3 +27,14 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+//Mongoose setup
+const PORT = process.env.PORT || 9000;
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+    .catch((err) => console.log(`${err} did not connect`));
