29bab712c27e96c8025d22a63461e0e21dda7b2d
Alex Wu <alexwu727@gmail.com>
2023-02-22 08:24:58 -0800

    geography page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3"
 
 export const getProducts = async (req, res) => {
     try {
@@ -29,7 +30,7 @@ export const getCustomers = async (req, res) => {
     }
 }
 
-// server-side pagination
+// transactions: server-side pagination
 export const getTransactions = async (req, res) => {
     try {
         const { page = 1, sort = null, pageSize = 20, search = "" } = req.query;
@@ -65,6 +66,31 @@ export const getTransactions = async (req, res) => {
         res.status(200).json(result);
     } catch (error) {
         console.log("error", error);
+        res.status(404).json({ message: error.message });
+    }
+}
+
+// get geography
+export const getGeography = async (req, res) => {
+    try {
+        const users = await User.find().select("-password");
+        // create countries count for nivo choropleth chart base on customers
+        const mappedLocations = users.reduce((acc, { country }) => {
+            const countryIso3 = getCountryIso3(country);
+            if (!acc[countryIso3]) {
+                acc[countryIso3] = 0;
+            }
+            acc[countryIso3] += 1;
+            return acc;
+        }, {});
+        const geography = Object.keys(mappedLocations).map((country) => {
+            return {
+                id: country,
+                value: mappedLocations[country]
+            }
+        })
+        res.status(200).json(geography);
+    } catch (error) {
         res.status(404).json({ message: error.message });
     }
 }
