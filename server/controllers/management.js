272122b1079d15e6661de9df16658e83f510b1c3
Alex Wu <alexwu727@gmail.com>
2023-02-23 21:07:44 -0800

    log error out

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -8,6 +8,7 @@ export const getAdmins = async (req, res) => {
         const admins = await User.find({ role: 'admin' }).select('-password');
         res.status(200).json(admins);
     } catch (error) {
+        console.log("error", error);
         res.status(404).json({ message: error.message });
     }
 }
